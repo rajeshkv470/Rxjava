d172c06ab5c7e532506c7194221bbfc1adb635b6
Rajesh Jakkula <Rajesh Jakkula>
2017-08-04 14:36:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jakku/weatherapp/AlertDialogFragment.java
@@ -0,0 +1,27 @@
+package com.example.jakku.weatherapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by jakku on 8/3/2017.
+ */
+
+public class AlertDialogFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Context context = getActivity();
+            AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                    .setTitle(context.getString(R.string.error_title))
+                    .setMessage(context.getString(R.string.error_message))
+                    .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+            AlertDialog dialog = builder.create();
+            return dialog;
+        }
+    }
+
